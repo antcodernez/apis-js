4b38c15fb5e8ee9165c42830e9b619146062217d
Antonio Jiménez <marshday5@gmail.com>
2023-07-10 20:35:15 -0600

    Modifided the DOM to render img about cats and fox using apis, and learned about query parameters just like breeds_ids=xxxx, and 10 pictures of cats

--- first-api/code.js
+++ first-api/code.js
@@ -1,4 +1,8 @@
-const API = " https://api.thecatapi.com/v1/images/search";
+const API = "https://api.thecatapi.com/v1/images/search";
+const API_quiery_limit_3 = "https://api.thecatapi.com/v1/images/search?limit=2&breed_ids=kora"; //Query parameter de la api con limite de 3 y usando el breed que se podria intuir que son las razas, la documentacion de la api dice que las breed_ids=xxxx son las primeras 4 letras de las razas
+//Liga ----> "https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=gpN-ReBkp"
+
+const api_ten_cats = "https://api.thecatapi.com/v1/images/search?limit=10";
 const foxsAPI = "https://randomfox.ca/floof/";
 
 const catImg = document.querySelector(".cats-image");
@@ -6,11 +10,20 @@ const btnGetCats = document.getElementById("button");
 
 function getMeACat()
     {          
-        fetch(API) //Recibe de argumento la URL de la API, como regresa una promesa puedo usar el método .then() 
+        fetch(api_ten_cats) //Recibe de argumento la URL de la API, como regresa una promesa puedo usar el método .then() 
         .then( res => res.json()) //Cuando cargo la API primero debo convertir mi respuesta a un objeto que pueda entender JS
-        .then(data => {
-            catImg.setAttribute("src",data[0].url);
-        })//Me devuelve una promesa, hago lo que quiera con mi respuesta de la API
+        .then(data => 
+            { 
+                for(cat of data)
+                    {   
+                        let imgCatTag = document.createElement("img");
+                        imgCatTag.classList.add("cats-image");
+                        imgCatTag.src = cat.url;
+                        // containerTag.appendChild(imgCatTag);   
+
+                        document.body.appendChild(imgCatTag);
+                    }
+            })//Me devuelve una promesa, hago lo que quiera con mi respuesta de la API
         .catch(error => console.log(error));
 
     }
@@ -31,7 +44,11 @@ async function getMeACatAsync(url)
         try
             {
                 const fox = await fetchData(url);
-                catImg.setAttribute("src", fox.image);
+                let imgCatTag = document.createElement("img");
+                imgCatTag.classList.add("cats-image");
+                imgCatTag.src = fox.image;
+                document.body.appendChild(imgCatTag);
+                
             }
         catch(error)
             {
@@ -46,5 +63,6 @@ btnDucks.addEventListener("click", () => {
 btnGetCats.addEventListener("click", getMeACat);
 
 // Son asincronas y en bucle
-setInterval(getMeACat, 10000);
-setInterval(() => getMeACatAsync(foxsAPI), 4000);
+// setInterval(getMeACat, 10000);
+// setInterval(() => getMeACatAsync(foxsAPI), 4000);
+
